cffa5368073f5497e9f3170f3e2b4c5616021473
TiagoDev23 <tiagojones11@gmail.com>
2025-10-11 19:27:49 -0300

    feat(button): cria componente genérico Button [TK-005]

--- odslize-game/src/App.js
+++ odslize-game/src/App.js
@@ -1,10 +1,15 @@
 import './styles/global.css';
+import Button from './components/common/Button';
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       <h1>Bem-vindo ao ODSlize!</h1>
-    </div>
+    </div><div>
+        <Button onClick={() => alert('Clicou!')}>Botão Primário</Button>
+        <Button variant="secondary">Botão Secundário</Button>
+        <Button disabled>Desabilitado</Button>
+      </div></>
   );
 }
 
